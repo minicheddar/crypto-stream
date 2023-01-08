24d1c12c6d7aac2a12247fe328952406857e06f5
ciaran <5202596+minicheddar@users.noreply.github.com>
2023-01-08 15:58:10 +0000

    factor out cross-exchange orderbook updates

--- examples/combine_books.rs
+++ examples/combine_books.rs
@@ -2,7 +2,6 @@ use mimalloc::MiMalloc;
 #[global_allocator]
 static GLOBAL: MiMalloc = MiMalloc;
 
-use chrono::Utc;
 use crossterm::{
     event::EnableMouseCapture,
     execute,
@@ -11,16 +10,13 @@ use crossterm::{
 use crypto_stream::{
     build_venue_subscriptions,
     model::*,
-    orderbook::{levels_to_orderbook, merge_orderbooks, Level, LimitOrderBook, Side},
+    orderbook::CrossVenueOrderBook,
     subscriptions_into_stream,
     tui::render_orderbook,
     websocket::{WebsocketSubscription, WebsocketSubscriptionKind},
 };
 use futures::StreamExt;
-use std::{
-    collections::{BTreeMap, HashMap},
-    io,
-};
+use std::{io, vec};
 use tui::{backend::CrosstermBackend, Terminal};
 
 #[tokio::main]
@@ -45,90 +41,31 @@ async fn main() {
     let venue_subs = build_venue_subscriptions(subscriptions);
     let mut market_data = subscriptions_into_stream(venue_subs).await;
 
-    let mut exchange_books: HashMap<Venue, LimitOrderBook> = HashMap::new();
-    const DEPTH: usize = 10;
-
     // setup terminal
-    enable_raw_mode();
+    let _ = enable_raw_mode();
     let mut stdout = io::stdout();
-    execute!(stdout, EnterAlternateScreen, EnableMouseCapture);
+    let _ = execute!(stdout, EnterAlternateScreen, EnableMouseCapture);
     let backend = CrosstermBackend::new(stdout);
     let mut terminal = Terminal::new(backend).unwrap();
 
+    let mut cross_book = CrossVenueOrderBook::new(10);
+
     while let Some(msg) = market_data.next().await {
-        let now = Utc::now();
-        match msg.kind {
-            MarketDataKind::L2Snapshot(snapshot) => {
-                let venue = msg.venue.clone();
-                let bids = levels_to_orderbook(&snapshot.bids, venue, Side::Bid, DEPTH);
-                let asks = levels_to_orderbook(&snapshot.asks, venue, Side::Ask, DEPTH);
-
-                exchange_books
-                    .entry(venue)
-                    .and_modify(|x| {
-                        x.bids = bids.clone();
-                        x.asks = asks.clone();
-                    })
-                    .or_insert(LimitOrderBook::new(bids, asks));
-            }
-            MarketDataKind::L2Update(update) => {
-                let venue = msg.venue.clone();
-                let bid_len = update.bids.len();
-                let bids = levels_to_orderbook(&update.bids, venue, Side::Bid, bid_len);
-
-                let ask_len = update.asks.len();
-                let asks = levels_to_orderbook(&update.asks, venue, Side::Ask, ask_len);
-
-                if let Some(ob) = exchange_books.get_mut(&venue) {
-                    ob.bids = merge_orderbooks(&ob.bids, &bids);
-                    ob.asks = merge_orderbooks(&ob.asks, &asks);
-                }
-            }
-            _ => continue,
-        }
-
-        let combined_book = exchange_books.values().fold(
-            LimitOrderBook::new(BTreeMap::new(), BTreeMap::new()),
-            |mut combined_book, ob| {
-                combined_book.bids.extend(ob.bids.clone());
-                combined_book.asks.extend(ob.asks.clone());
-                combined_book
-            },
-        );
-
-        // println!("");
-        // println!("Combined bids BEFORE: {:?}", combined_book.bids);
-        // println!("Combined asks BEFORE: {:?}", combined_book.asks);
-
-        let asks: Vec<&Level> = combined_book
-            .asks
-            .values()
-            .into_iter()
-            .take(DEPTH)
-            .collect();
-
-        let bids: Vec<&Level> = combined_book
-            .bids
-            .values()
-            .into_iter()
-            .rev()
-            .take(DEPTH)
-            .collect();
-
-        let levels: Vec<&Level> = asks.into_iter().chain(bids.into_iter()).collect();
-        // let took = (Utc::now() - now).num_microseconds().unwrap();
+        cross_book.update(&msg);
+        let levels = cross_book.to_levels(cross_book.depth);
 
         terminal
             .draw(|f| render_orderbook(f, levels))
             .expect("error rendering TUI");
-
-        // if let Event::Key(key) = event::read()? {
-        //     match key.code {
-        //         KeyCode::Char('q') => return Ok(()),
-        //         // KeyCode::Down => app.next(),
-        //         // KeyCode::Up => app.previous(),
-        //         _ => continue
-        //     }
-        // }
     }
+
+    // // listen to exit event
+    // if let Event::Key(key) = event::read()? {
+    //     match key.code {
+    //         KeyCode::Char('q') => return Ok(()),
+    //         // KeyCode::Down => app.next(),
+    //        // KeyCode::Up => app.previous(),
+    //         _ => continue
+    //     }
+    // }
 }
--- src/orderbook.rs
+++ src/orderbook.rs
@@ -1,7 +1,18 @@
-use crate::model::{OrderBookLevel, Venue};
+use crate::model::{MarketData, MarketDataKind, OrderBookLevel, Venue};
 use rust_decimal::{prelude::FromPrimitive, Decimal};
 use rust_decimal_macros::dec;
-use std::{cmp::Ordering, collections::BTreeMap};
+use std::{
+    cmp::Ordering,
+    collections::{BTreeMap, HashMap},
+};
+
+type LevelMap = BTreeMap<Decimal, Level>;
+
+#[derive(Clone, Copy, PartialEq, Eq, Debug)]
+pub enum Side {
+    Bid,
+    Ask,
+}
 
 #[derive(Clone, PartialEq, Eq, Debug)]
 pub struct Level {
@@ -14,12 +25,6 @@ pub struct Level {
     pub side: Side,
 }
 
-#[derive(Clone, Copy, PartialEq, Eq, Debug)]
-pub enum Side {
-    Bid,
-    Ask,
-}
-
 impl PartialOrd for Level {
     fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
         match self.price.partial_cmp(&other.price) {
@@ -29,21 +34,7 @@ impl PartialOrd for Level {
     }
 }
 
-type LevelMap = BTreeMap<Decimal, Level>;
-
-#[derive(PartialEq, Debug)]
-pub struct LimitOrderBook {
-    pub bids: LevelMap,
-    pub asks: LevelMap,
-}
-
-impl LimitOrderBook {
-    pub fn new(bids: LevelMap, asks: LevelMap) -> Self {
-        LimitOrderBook { bids, asks }
-    }
-}
-
-pub fn levels_to_orderbook(
+pub fn levels_to_map(
     levels: &Vec<OrderBookLevel>,
     venue: Venue,
     side: Side,
@@ -67,7 +58,7 @@ pub fn levels_to_orderbook(
         .collect::<BTreeMap<Decimal, Level>>()
 }
 
-pub fn merge_orderbooks(a: &LevelMap, b: &LevelMap) -> LevelMap {
+pub fn merge_maps(a: &LevelMap, b: &LevelMap) -> LevelMap {
     let mut book = a
         .into_iter()
         .chain(b)
@@ -79,4 +70,91 @@ pub fn merge_orderbooks(a: &LevelMap, b: &LevelMap) -> LevelMap {
         });
     book.retain(|_, level| level.quantity != dec!(0));
     book
+}
+
+#[derive(PartialEq, Debug)]
+pub struct LimitOrderBook {
+    pub bids: LevelMap,
+    pub asks: LevelMap,
+}
+
+impl LimitOrderBook {
+    pub fn new(bids: LevelMap, asks: LevelMap) -> Self {
+        LimitOrderBook { bids, asks }
+    }
+}
+
+pub struct CrossVenueOrderBook {
+    pub depth: usize,
+    exchange_books: HashMap<Venue, LimitOrderBook>,
+    combined_book: LimitOrderBook,
+}
+
+impl CrossVenueOrderBook {
+    pub fn new(depth: usize) -> Self {
+        Self {
+            depth,
+            exchange_books: HashMap::new(),
+            combined_book: LimitOrderBook::new(BTreeMap::new(), BTreeMap::new()),
+        }
+    }
+
+    pub fn update(&mut self, msg: &MarketData) {
+        match &msg.kind {
+            MarketDataKind::L2Snapshot(snapshot) => {
+                let venue = msg.venue.clone();
+                let bids = levels_to_map(&snapshot.bids, venue, Side::Bid, self.depth);
+                let asks = levels_to_map(&snapshot.asks, venue, Side::Ask, self.depth);
+
+                self.exchange_books
+                    .insert(venue, LimitOrderBook::new(bids, asks));
+            }
+            MarketDataKind::L2Update(update) => {
+                let venue = msg.venue.clone();
+                let bid_len = update.bids.len();
+                let bids = levels_to_map(&update.bids, venue, Side::Bid, bid_len);
+
+                let ask_len = update.asks.len();
+                let asks = levels_to_map(&update.asks, venue, Side::Ask, ask_len);
+
+                if let Some(ob) = self.exchange_books.get_mut(&venue) {
+                    ob.bids = merge_maps(&ob.bids, &bids);
+                    ob.asks = merge_maps(&ob.asks, &asks);
+                }
+            }
+            _ => return,
+        }
+
+        self.combined_book = self.exchange_books.values().fold(
+            LimitOrderBook::new(BTreeMap::new(), BTreeMap::new()),
+            |mut combined_book, ob| {
+                combined_book.bids.extend(ob.bids.clone());
+                combined_book.asks.extend(ob.asks.clone());
+                combined_book
+            },
+        );
+    }
+
+    pub fn to_levels(&self, depth: usize) -> Vec<&Level> {
+        let asks: Vec<&Level> = self
+            .combined_book
+            .asks
+            .values()
+            .into_iter()
+            .take(depth)
+            // .cloned()
+            .collect();
+
+        let bids: Vec<&Level> = self
+            .combined_book
+            .bids
+            .values()
+            .into_iter()
+            .rev()
+            .take(depth)
+            // .cloned()
+            .collect();
+
+        return asks.into_iter().chain(bids.into_iter()).collect();
+    }
 }
