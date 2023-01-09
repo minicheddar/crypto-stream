565d90f1bcd18d22492b741b2bc8439bea297717
ciaran <5202596+minicheddar@users.noreply.github.com>
2023-01-09 11:37:06 +0000

    add orderbook.merge_maps() tests

--- src/orderbook.rs
+++ src/orderbook.rs
@@ -63,6 +63,7 @@ pub fn merge_maps(a: &LevelMap, b: &LevelMap) -> LevelMap {
         .chain(b)
         .fold(BTreeMap::new(), |mut map, (price, level)| {
             map.entry(*price)
+                // TODO: The venue will always be from LevelMap 'a'. Need to decide which venue it should be (e.g. one with highest volume at that level?)
                 .and_modify(|l: &mut Level| l.quantity += level.quantity)
                 .or_insert(level.clone());
             map
@@ -258,5 +259,221 @@ mod tests {
         .collect();
 
         assert_eq!(levels_to_map(&input, Venue::Coinbase, Side::Bid, 2), output);
+    }
+
+    #[test]
+    fn merge_maps_combines_levels_from_multiple_venues() {
+        let coinbase_book: LevelMap = [
+            (
+                Decimal::from_f64(100.0).unwrap(),
+                Level {
+                    price: Decimal::from_f64(100.0).unwrap(),
+                    quantity: Decimal::from_f64(10.0).unwrap(),
+                    venue: Venue::Coinbase,
+                    side: Side::Bid,
+                },
+            ),
+            (
+                Decimal::from_f64(110.0).unwrap(),
+                Level {
+                    price: Decimal::from_f64(110.0).unwrap(),
+                    quantity: Decimal::from_f64(20.0).unwrap(),
+                    venue: Venue::Coinbase,
+                    side: Side::Bid,
+                },
+            ),
+        ]
+        .iter()
+        .cloned()
+        .collect();
+
+        let binance_book: LevelMap = [
+            (
+                Decimal::from_f64(110.0).unwrap(),
+                Level {
+                    price: Decimal::from_f64(110.0).unwrap(),
+                    quantity: Decimal::from_f64(30.0).unwrap(),
+                    venue: Venue::BinanceSpot,
+                    side: Side::Bid,
+                },
+            ),
+            (
+                Decimal::from_f64(120.0).unwrap(),
+                Level {
+                    price: Decimal::from_f64(120.0).unwrap(),
+                    quantity: Decimal::from_f64(40.0).unwrap(),
+                    venue: Venue::BinanceSpot,
+                    side: Side::Bid,
+                },
+            ),
+        ]
+        .iter()
+        .cloned()
+        .collect();
+
+        let output: LevelMap = [
+            (
+                Decimal::from_f64(100.0).unwrap(),
+                Level {
+                    price: Decimal::from_f64(100.0).unwrap(),
+                    quantity: Decimal::from_f64(10.0).unwrap(),
+                    venue: Venue::Coinbase,
+                    side: Side::Bid,
+                },
+            ),
+            (
+                Decimal::from_f64(110.0).unwrap(),
+                Level {
+                    price: Decimal::from_f64(110.0).unwrap(),
+                    quantity: Decimal::from_f64(50.0).unwrap(),
+                    venue: Venue::Coinbase,
+                    side: Side::Bid,
+                },
+            ),
+            (
+                Decimal::from_f64(120.0).unwrap(),
+                Level {
+                    price: Decimal::from_f64(120.0).unwrap(),
+                    quantity: Decimal::from_f64(40.0).unwrap(),
+                    venue: Venue::BinanceSpot,
+                    side: Side::Bid,
+                },
+            ),
+        ]
+        .iter()
+        .cloned()
+        .collect();
+
+        assert_eq!(merge_maps(&coinbase_book, &binance_book), output);
+    }
+
+    #[test]
+    fn merge_maps_removes_zero_quantity_levels() {
+        let mut coinbase_book: LevelMap = [
+            (
+                Decimal::from_f64(100.0).unwrap(),
+                Level {
+                    price: Decimal::from_f64(100.0).unwrap(),
+                    quantity: Decimal::from_f64(10.0).unwrap(),
+                    venue: Venue::Coinbase,
+                    side: Side::Ask,
+                },
+            ),
+            (
+                Decimal::from_f64(110.0).unwrap(),
+                Level {
+                    price: Decimal::from_f64(110.0).unwrap(),
+                    quantity: dec!(0),
+                    venue: Venue::Coinbase,
+                    side: Side::Ask,
+                },
+            ),
+        ]
+        .iter()
+        .cloned()
+        .collect();
+
+        let binance_book: LevelMap = [
+            (
+                Decimal::from_f64(110.0).unwrap(),
+                Level {
+                    price: Decimal::from_f64(110.0).unwrap(),
+                    quantity: Decimal::from_f64(30.0).unwrap(),
+                    venue: Venue::BinanceSpot,
+                    side: Side::Ask,
+                },
+            ),
+            (
+                Decimal::from_f64(115.0).unwrap(),
+                Level {
+                    price: Decimal::from_f64(115.0).unwrap(),
+                    quantity: Decimal::from_f64(25.0).unwrap(),
+                    venue: Venue::BinanceSpot,
+                    side: Side::Ask,
+                },
+            ),
+            (
+                Decimal::from_f64(120.0).unwrap(),
+                Level {
+                    price: Decimal::from_f64(120.0).unwrap(),
+                    quantity: dec!(0),
+                    venue: Venue::BinanceSpot,
+                    side: Side::Ask,
+                },
+            ),
+        ]
+        .iter()
+        .cloned()
+        .collect();
+
+        let combined_book_1: LevelMap = [
+            (
+                Decimal::from_f64(100.0).unwrap(),
+                Level {
+                    price: Decimal::from_f64(100.0).unwrap(),
+                    quantity: Decimal::from_f64(10.0).unwrap(),
+                    venue: Venue::Coinbase,
+                    side: Side::Ask,
+                },
+            ),
+            (
+                Decimal::from_f64(110.0).unwrap(),
+                Level {
+                    price: Decimal::from_f64(110.0).unwrap(),
+                    quantity: Decimal::from_f64(30.0).unwrap(),
+                    venue: Venue::Coinbase,
+                    side: Side::Ask,
+                },
+            ),
+            (
+                Decimal::from_f64(115.0).unwrap(),
+                Level {
+                    price: Decimal::from_f64(115.0).unwrap(),
+                    quantity: Decimal::from_f64(25.0).unwrap(),
+                    venue: Venue::BinanceSpot,
+                    side: Side::Ask,
+                },
+            ),
+        ]
+        .iter()
+        .cloned()
+        .collect();
+
+        assert_eq!(merge_maps(&coinbase_book, &binance_book), combined_book_1);
+
+        coinbase_book.insert(
+            Decimal::from_f64(100.0).unwrap(),
+            Level {
+                price: Decimal::from_f64(100.0).unwrap(),
+                quantity: dec!(0),
+                venue: Venue::Coinbase,
+                side: Side::Ask,
+            },
+        );
+        let combined_book_2: LevelMap = [
+            (
+                Decimal::from_f64(110.0).unwrap(),
+                Level {
+                    price: Decimal::from_f64(110.0).unwrap(),
+                    quantity: Decimal::from_f64(30.0).unwrap(),
+                    venue: Venue::Coinbase,
+                    side: Side::Ask,
+                },
+            ),
+            (
+                Decimal::from_f64(115.0).unwrap(),
+                Level {
+                    price: Decimal::from_f64(115.0).unwrap(),
+                    quantity: Decimal::from_f64(25.0).unwrap(),
+                    venue: Venue::BinanceSpot,
+                    side: Side::Ask,
+                },
+            ),
+        ]
+        .iter()
+        .cloned()
+        .collect();
+
+        assert_eq!(merge_maps(&coinbase_book, &binance_book), combined_book_2);
     }
 }
